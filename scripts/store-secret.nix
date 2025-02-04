0152d250fa1c30affe316d5b0d617c68fa83d887
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2025-02-04 17:04:50 +0100

    Don't overwrite if secret exists.

--- scripts/store-secret.nix
+++ scripts/store-secret.nix
@@ -3,13 +3,20 @@ let
   curl = "${pkgs.curl}/bin/curl";
   openssl = "${pkgs.openssl}/bin/openssl";
 in pkgs.writeShellScriptBin "store_secret" ''
-   export NEW_SECRET=$(${openssl} rand -base64 20) && \
-     echo "Generated $NEW_SECRET (stored under $TEST_SECRET_NAME)" && \
-     curl \
-       --header "X-Vault-Token: $OPENBAO_TOKEN" \
-       --header "Content-Type: application/json" \
-       --request POST \
-       --data "{\"data\": {\"password\": \"$NEW_SECRET\"}}" \
-       $BAO_ADDR/v1/secret/data/$TEST_SECRET_NAME && \
-      echo "Secret $TEST_SECRET_NAME written successfully."
+   export URL=$BAO_ADDR/v1/secret/data/$TEST_SECRET_NAME
+   code=$(${curl} -H "x-vault-token: $OPENBAO_TOKEN" -s $URL --write-out '%{http_code}' -o /dev/null) # check if the secret exists. 
+   echo "Secret check: $code"
+   if [[ $code != 200 ]]; then
+     export NEW_SECRET=$(${openssl} rand -base64 20) && \
+       echo "Storing $NEW_SECRET as $TEST_SECRET_NAME" && \
+       ${curl} \
+         --header "X-Vault-Token: $OPENBAO_TOKEN" \
+         --header "Content-Type: application/json" \
+         --request POST \
+         --data "{\"data\": {\"password\": \"$NEW_SECRET\"}}" \
+         $URL && \
+        echo "Secret $TEST_SECRET_NAME written successfully"
+   else
+     echo "$TEST_SECRET_NAME exists, nothing to do"
+   fi 
 '' 
