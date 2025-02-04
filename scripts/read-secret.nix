88bfebc84ad18c49a80ea9e79a02335543e53bcb
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2025-02-04 17:12:40 +0100

    Filter out redundant info with jq.

--- scripts/read-secret.nix
+++ scripts/read-secret.nix
@@ -1,8 +1,10 @@
 { pkgs ? (import (import ../nix/sources.nix).nixpkgs {}) }:
 let
   curl = "${pkgs.curl}/bin/curl";
+  jq = "${pkgs.jq}/bin/jq";
 in pkgs.writeShellScriptBin "read_secret" ''
    ${curl} \
      --header "X-Vault-Token: $OPENBAO_TOKEN" \
-     $BAO_ADDR/v1/secret/data/$TEST_SECRET_NAME 
+     $BAO_ADDR/v1/secret/data/$TEST_SECRET_NAME \
+     | ${jq} ".data.data.password"
 '' 
